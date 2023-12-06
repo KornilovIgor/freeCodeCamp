be16dfe6413b2897170cd1ff07fdc7b8b9260018
KornilovIgor <i9218452052@gmail.com>
2023-12-06 20:01:54 +0300

    "Modify Array Data With Indexes" is done.

---
+++ modify-array-data-with-indexes.js
@@ -0,0 +1,13 @@
+//❓DESCRIPTION:
+/*
+Modify the data stored at index 0 of myArray to a value of 45.
+
+// Setup
+const myArray = [18, 64, 99];
+
+// Only change code below this line
+*/
+
+//✅SOLUTION:
+const myArray = [18, 64, 99];
+myArray[0] = 45;
