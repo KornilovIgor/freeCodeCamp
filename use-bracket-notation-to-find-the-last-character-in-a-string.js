d75bacfcc55c19aead53e1c3fb9d8c535a8ec79e
KornilovIgor <i9218452052@gmail.com>
2023-12-06 18:59:20 +0300

    "Find the Last Character in a String" is done.

---
+++ use-bracket-notation-to-find-the-last-character-in-a-string.js
@@ -0,0 +1,14 @@
+//❓DESCRIPTION:
+/*
+Use bracket notation to find the last character in the lastName variable.
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName; // Change this line
+*/
+
+//✅SOLUTION:
+const lastName = "Lovelace";
+const lastLetterOfLastName = lastName[lastName.length - 1];
