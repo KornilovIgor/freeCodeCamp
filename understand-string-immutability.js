0c18e07aa02a53d8874d0493214f8c6d59014dd7
KornilovIgor <i9218452052@gmail.com>
2023-12-06 18:53:19 +0300

    "Understand String Immutability" is done.

---
+++ understand-string-immutability.js
@@ -0,0 +1,17 @@
+//❓DESCRIPTION:
+/*
+Correct the assignment to myStr so it contains the string value of Hello World using
+the approach shown in the example above.
+
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr[0] = "H"; // Change this line
+// Only change code above this line
+*/
+
+//✅SOLUTION:
+let myStr = "Jello World";
+
+myStr = "Hello World";
