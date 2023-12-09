2fa5e5ab9a4ed67d48f489267438d67e2a7ae812
KornilovIgor <i9218452052@gmail.com>
2023-12-10 01:10:02 +0300

    "Understanding Boolean Values" is done.

---
+++ understanding-boolean-values.js
@@ -0,0 +1,16 @@
+// ❓DESCRIPTION:
+//Modify the welcomeToBooleans function so that it returns true instead of false.
+//
+//function welcomeToBooleans() {
+//   // Only change code below this line
+//
+//   return false; // Change this line
+//
+//   // Only change code above this line
+// }
+
+// ✅SOLUTION:
+function welcomeToBooleans()
+{
+    return true;
+}
