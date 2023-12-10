1852d915130e0f8f60ec3abe3fe1587295ed1662
KornilovIgor <i9218452052@gmail.com>
2023-12-10 23:47:51 +0300

    "Comparison with the Equality Operator" is done.

---
+++ comparison-with-the-equality-operator.js
@@ -0,0 +1,22 @@
+// ❓DESCRIPTION:
+//Add the equality operator to the indicated line so that the function will return the string Equal when val is equivalent to 12.
+// // Setup
+// function testEqual(val) {
+//     if (val) { // Change this line
+//         return "Equal";
+//     }
+//     return "Not Equal";
+// }
+//
+// testEqual(10);
+
+// ✅SOLUTION:
+function testEqual(val) {
+    if (val == 12)
+    {
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+testEqual(10);
