7cb4863c13ed0f88313fcfbf3cd1078a8018ab5b
KornilovIgor <mail@kote.dev>
2023-12-12 01:31:14 +0300

    "Else-If" is done.

---
+++ introducing-else-if-statements.js
@@ -0,0 +1,36 @@
+// ❓DESCRIPTION:
+//Convert the logic to use else if statements.
+/*
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+
+  if (val < 5) {
+    return "Smaller than 5";
+  }
+
+  return "Between 5 and 10";
+}
+
+testElseIf(7);
+*/
+
+// ✅SOLUTION:
+function testElseIf(val)
+{
+    if (val > 10)
+    {
+        return "Greater than 10";
+    }
+    else if (val < 5)
+    {
+        return "Smaller than 5";
+    }
+    else
+    {
+        return "Between 5 and 10";
+    }
+}
+
+testElseIf(7);
