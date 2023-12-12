e4c429ff8cc39ce72b8fedb67da01cc13883963d
KornilovIgor <mail@kote.dev>
2023-12-12 13:18:42 +0300

    "Return Early Pattern for Functions" is done.

---
+++ return-early-pattern-for-functions.js
@@ -0,0 +1,16 @@
+// ❓DESCRIPTION:
+/*
+Modify the function abTest so that if a or b are less than 0 the function will
+immediately exit with a value of undefined.
+*/
+
+// ✅SOLUTION:
+function abTest(a, b)
+{
+    if (a < 0 || b < 0)
+    {
+        return undefined;
+    }
+
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
