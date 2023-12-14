4472c54998f319b2d06a5786b419b47203c70610
KornilovIgor <mail@kote.dev>
2023-12-14 21:29:48 +0300

    "Generate Random Whole Numbers with JS" is done.

---
+++ generate-random-whole-numbers-with-javascript.js
@@ -0,0 +1,14 @@
+// ❓DESCRIPTION:
+/*
+Use this technique to generate and return a random whole number in the range from 0
+to 9.
+
+function randomWholeNum() {
+  return Math.random();
+}
+*/
+
+// ✅SOLUTION:
+function randomWholeNum() {
+    return Math.floor(Math.random() * 10);
+}
