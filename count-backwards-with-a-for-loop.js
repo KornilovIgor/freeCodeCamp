814df71c9f710d313f63b50369f1e367860c42ba
KornilovIgor <mail@kote.dev>
2023-12-14 19:33:31 +0300

    "Count Backwards With a For Loop" is done.

---
+++ count-backwards-with-a-for-loop.js
@@ -0,0 +1,11 @@
+// ❓DESCRIPTION:
+/*
+Push the odd numbers from 9 through 1 to myArray using a for loop.
+*/
+
+// ✅SOLUTION:
+const myArray = [];
+
+for (let i = 9; i >= 1; i -= 2){
+    myArray.push(i);
+}
