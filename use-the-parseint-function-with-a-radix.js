314af64a532ec48c65e82067f78f0e902683b63b
KornilovIgor <mail@kote.dev>
2023-12-14 21:43:03 +0300

    "Use the parseInt Function with a Radix" is done.

---
+++ use-the-parseint-function-with-a-radix.js
@@ -0,0 +1,20 @@
+// ❓DESCRIPTION:
+/*
+Use parseInt() in the convertToInteger function so it converts a binary number to an
+integer and returns it.
+
+function convertToInteger(str) {
+
+}
+
+convertToInteger("10011");
+*/
+
+// ✅SOLUTION:
+function convertToInteger(str) {
+
+    return parseInt(str, 2);
+
+}
+
+convertToInteger("10011");
